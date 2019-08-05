85bfdf1b31c075aa86e9c8af17314f0771582a85
David Strauch <dmaster18@gmail.com>
2019-08-05 18:42:42 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,6 +15,8 @@ contacts = {
 
 def remove_strawberry(contacts)
  contacts.each do |person, contact_details_hash|
+   if person == "Freddy Mercury"
+     contact_details_hash.each do |attribute, data|
    binding.pry
   end
 end
