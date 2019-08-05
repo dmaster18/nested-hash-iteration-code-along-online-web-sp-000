eb5b5ba59b686de5bcc66d4dc413d4f280ac6dd7
David Strauch <dmaster18@gmail.com>
2019-08-05 18:25:52 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,7 @@ contacts = {
    }
   
 def remove_strawberry(contacts)
-  delete contacts["Freddie Mercury"][:favorite_ice_cream_flavors][0]
+  contacts["Freddie Mercury"][:favorite_ice_cream_flavors][0]
 end
 
 puts contacts
