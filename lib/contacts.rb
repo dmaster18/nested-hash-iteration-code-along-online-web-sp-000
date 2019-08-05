2666b295f3d5704d02c2f88583aa4a6f9bbd9484
David Strauch <dmaster18@gmail.com>
2019-08-05 18:33:38 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -12,7 +12,7 @@ contacts = {
     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   }
    }
-puts contacts["Freddie Mercury"].class
+
 def remove_strawberry(contacts)
   contacts["Freddie Mercury"][:favorite_ice_cream_flavors][0]
 end
